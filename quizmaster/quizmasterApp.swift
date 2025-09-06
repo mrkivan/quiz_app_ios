df591c66c89ff08f0d7b490062b5c813ed6c8e1e
Riazul Karim Ivan <ivancse58@gmail.com>
2025-09-06 15:51:27 +0600

    Initial Commit

---
+++ quizmaster/quizmasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizmasterApp.swift
+//  quizmaster
+//
+//  Created by Riazul Karim on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct quizmasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quizmasterTests/quizmasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  quizmasterTests.swift
+//  quizmasterTests
+//
+//  Created by Riazul Karim on 6/9/25.
+//
+
+import Testing
+@testable import quizmaster
+
+struct quizmasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
